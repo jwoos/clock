7070da5021d917ff969bd6567f33c5522a0b4f3f
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-02-06 23:12:52 -0500

    yay

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+	var temp = moment(),
+		tempSecond = temp.seconds() * 6,
+		tempMinute = temp.minutes() * 6 + second / 60,
+		tempHour = ((temp.hours() % 12) / 12) * 360 + 90 + minute / 12;
+	$('#minute-hand').css('transform', 'rotateZ(' + tempMinute + 'deg)');
+	$('#hour-hand').css('transform', 'rotateZ(' + tempHour + 'deg)');
+	$('#second').css('transform', 'rotateZ(' + tempSecond + 'deg)');
+
 	function updateClock() {
 		var now = moment(),
 			second = now.seconds() * 6,
 			minute = now.minutes() * 6 + second / 60,
 			hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
 
-		new TweenMax('path', 2, {delay: 0, Ease: 'Linear', rotation: '+=6', rotation: '10deg', transformOrigin: '0% 100%'});
+		$('#minute-hand').css('transform', 'rotateZ(' + minute + 'deg)');
+		$('#hour-hand').css('transform', 'rotateZ(' + hour + 'deg)');
+		var animation = new TimelineMax();
+		animation.to('#second-hand', 0, {className: 'float', ease: Linear.easeNone, transformOrigin: '0% 100%'})
+		         .to('#second-hand-path', 0.3, {delay: 0, ease: Linear.easeNone, rotation: '+=6', transformOrigin: '0% 100%'})
+		         .to('#second-hand', 0, {className: '', ease: Linear.easeNone, transformOrigin: '0% 100%'})
 	}
 
 	function timedUpdate() {
@@ -13,5 +26,5 @@ $(document).ready(function() {
 		setTimeout(timedUpdate, 1000);
 	}
 
-	timedUpdate();
+	setTimeout(timedUpdate, 1000);
 });
